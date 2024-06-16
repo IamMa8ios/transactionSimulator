8efb443e37fedb7752e757ba1f4020d55cca6ea4
IamMa8ios <ma8ios95@gmail.com>
2024-06-16 22:39:11 +0300

    Aggregated generators into their own repos, services & routers Created generic CRUD routers Updated Customer & Customer Personal Info routers

--- api/v3/customer/routers/customer.py
+++ api/v3/customer/routers/customer.py
@@ -1,50 +1,11 @@
-from typing import List, Annotated
-from uuid import UUID
-
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter
 
 from api.v3.customer.dependencies import get_customer_service
 from api.v3.customer.schemas import CustomerRead, CustomerCreate, CustomerUpdate
 from api.v3.customer.services import CustomerService
+from api.v3.templates import add_crud_routes
 
 router = APIRouter(prefix="/api/v3/customer", tags=["Customer Management"])
 
 
-@router.get("/all", response_model=List[CustomerRead])
-async def get_all_customers(customer_service: Annotated[CustomerService, Depends(get_customer_service)]):
-    return customer_service.get_all()
-
-
-@router.get("/{customer_id}", response_model=CustomerRead)
-async def get_customer(customer_id: UUID, customer_service: Annotated[CustomerService, Depends(get_customer_service)]):
-    return customer_service.get_by_id(customer_id)
-
-
-@router.get("/random", response_model=CustomerRead)
-async def get_random_customer(customer_service: Annotated[CustomerService, Depends(get_customer_service)]):
-    return customer_service.get_random()
-
-
-@router.get("/email/{email}", response_model=CustomerRead)
-async def get_customer_by_email(email: str, customer_service: Annotated[CustomerService,
-                                Depends(get_customer_service)]):
-    return customer_service.get_by_email(email=email)
-
-
-@router.post("", response_model=CustomerRead, dependencies=[Depends(get_customer_service)],
-             status_code=status.HTTP_201_CREATED)
-async def create_customer(customer: CustomerCreate, customer_service: Annotated[CustomerService,
-                          Depends(get_customer_service)]):
-    return customer_service.create(customer)
-
-
-@router.put("/{customer_id}", response_model=CustomerRead)
-async def update_customer(customer_id: UUID, customer_data: CustomerUpdate,
-                          customer_service: Annotated[CustomerService, Depends(get_customer_service)]):
-    return customer_service.update(customer_id, customer_data)
-
-
-@router.delete("/{customer_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_customer(customer_id: UUID, customer_service: Annotated[CustomerService,
-                          Depends(get_customer_service)]):
-    return customer_service.delete(customer_id)
+add_crud_routes(router, CustomerService, CustomerCreate, CustomerRead, CustomerUpdate, get_customer_service)
--- api/v3/customer/routers/customer_personal_info.py
+++ api/v3/customer/routers/customer_personal_info.py
@@ -1,49 +1,12 @@
-from typing import List, Annotated
-from uuid import UUID
-
 from fastapi import APIRouter, Depends, status
 
 from api.v3.customer.dependencies import get_customer_personal_info_service
 from api.v3.customer.schemas import CustomerPersonalInfoCreate, CustomerPersonalInfoRead, CustomerPersonalInfoUpdate
 from api.v3.customer.services import CustomerPersonalInfoService
+from api.v3.templates import add_crud_routes
 
 router = APIRouter(prefix="/api/v3/personal_info", tags=["Customer's Personal Info Management"])
 
 
-@router.get("/all", response_model=List[CustomerPersonalInfoRead])
-async def get_all_customer_personal_info(customer_personal_info_service: Annotated[CustomerPersonalInfoService, 
-                                         Depends(get_customer_personal_info_service)]):
-    return customer_personal_info_service.get_all()
-
-
-@router.get("/{customer_personal_info_id}", response_model=CustomerPersonalInfoRead)
-async def get_customer(customer_personal_info_id: UUID, customer_personal_info_service:
-                       Annotated[CustomerPersonalInfoService, Depends(get_customer_personal_info_service)]):
-    return customer_personal_info_service.get_by_id(customer_personal_info_id)
-
-
-@router.get("/customer/{customer_id}", response_model=CustomerPersonalInfoRead)
-async def get_by_customer_id(customer_id: UUID, customer_personal_info_service: Annotated[CustomerPersonalInfoService,
-                             Depends(get_customer_personal_info_service)]):
-    return customer_personal_info_service.get_by_customer_id(customer_id)
-
-
-@router.post("", response_model=CustomerPersonalInfoRead,
-             dependencies=[Depends(get_customer_personal_info_service)], status_code=status.HTTP_201_CREATED)
-async def create_customer_personal_info(customer_personal_info: CustomerPersonalInfoCreate,
-                                        customer_personal_info_service: Annotated[CustomerPersonalInfoService, Depends(
-                                            get_customer_personal_info_service)]):
-    return customer_personal_info_service.create(customer_personal_info)
-
-
-@router.put("/{customer_personal_info_id}", response_model=CustomerPersonalInfoRead)
-async def update_customer_personal_info(customer_personal_info_id: UUID, customer_data: CustomerPersonalInfoUpdate,
-                                        customer_personal_info_service: Annotated[
-                                            CustomerPersonalInfoService, Depends(get_customer_personal_info_service)]):
-    return customer_personal_info_service.update(customer_personal_info_id, customer_data)
-
-
-@router.delete("/{customer_personal_info_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_customer_personal_info(customer_personal_info_id: UUID, customer_personal_info_service: Annotated[
-                                        CustomerPersonalInfoService, Depends(get_customer_personal_info_service)]):
-    return customer_personal_info_service.delete(customer_personal_info_id)
+add_crud_routes(router, CustomerPersonalInfoService, CustomerPersonalInfoCreate, CustomerPersonalInfoRead,
+                CustomerPersonalInfoUpdate, get_customer_personal_info_service)
